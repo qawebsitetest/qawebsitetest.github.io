da15b36c6fd3c4ca9a6f04044db6862633c6060b
Zivorad Trickovic <zivoradtrickovic@gmail.com>
2023-12-25 23:34:04 +0100

    v2.16

---
+++ js/pages/contact/typeFeature.js
@@ -0,0 +1,102 @@
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function dynamicTextTyping(options) {
+    const { targetClass, text, delay, cursorSpeed } = options;
+
+    const div = document.querySelector(`.${targetClass}`);
+    div.innerHTML = "";
+    const cursor = document.createElement("span");
+    cursor.textContent = "|";
+    const cursorBlinkSpeed = 800;
+
+    let i = 0;
+
+    function addNewLines() {
+        // Add new lines after specific parts of the text
+        const currentSubstring = text.substring(0, i);
+        const parts = [
+            'B2B & Full Stack Marketing agency',
+            'B2B & Full Stack Marketing agency Digital Advertising',
+            'B2B & Full Stack Marketing agency Digital Advertising Web Development'
+        ];
+
+        parts.forEach((part) => {
+            if (currentSubstring === part) {
+                div.innerHTML += '<br>';
+            }
+        });
+    }
+
+    function textTypingWithDelayAndNewLines() {
+        if (i < text.length) {
+            if (i !== 0) {
+                div.removeChild(cursor);
+            }
+            addNewLines();
+            div.innerHTML += text[i];
+            div.appendChild(cursor);
+
+            // Change cursor opacity with animation
+            cursor.style.transition = `opacity ${cursorSpeed / 2 / 1000}s`;
+            cursor.style.opacity = 0.3;
+
+            setTimeout(() => {
+                cursor.style.opacity = 1;
+            }, cursorSpeed / 2);
+
+            i++;
+            setTimeout(textTypingWithDelayAndNewLines, delay);
+        } else {
+            // Continuously change cursor opacity
+            setInterval(() => {
+                cursor.style.transition = `opacity ${cursorSpeed / 2 / 1000}s`;
+                cursor.style.opacity = 0.3;
+
+                setTimeout(() => {
+                    cursor.style.opacity = 1;
+                }, cursorSpeed / 2);
+            }, cursorBlinkSpeed);
+        }
+    }
+
+    // Intersection Observer setup
+    const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Element is in the viewport, start typing after a delay
+            cursor.style.visibility = "visible"; // Show the cursor
+            
+            // Introduce a delay before starting the typing animation
+            setTimeout(() => {
+                textTypingWithDelayAndNewLines();
+                observer.unobserve(div); // Stop observing once triggered
+            }, 500); // 900 milliseconds (0.9 seconds) delay
+        }
+    });
+});
+
+    // Start observing the target element
+    observer.observe(div);
+}
+
+
+
+const options9 = {
+    targetClass: "display-4",
+    text: "Feel free to contact us for any project idea or collaboration",
+    delay: 80,
+    cursorSpeed: 900
+};
+
+// Start observing the target elements
+
+
+dynamicTextTyping(options9);
---
+++ mail.php
@@ -0,0 +1,62 @@
+<?php
+
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$subject = $_POST["subject"];
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './phpmailer/src/Exception.php';
+require './phpmailer/src/PHPMailer.php';
+require './phpmailer/src/SMTP.php';
+
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'zivoradtrickovic@gmail.com';                     //SMTP username
+    $mail->Password   = 'qaprdezvakcbhdel';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('zivoradtrickovic@gmail.com');     //Add a recipient
+              //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    /$mail->smtpConnect(
+    array(
+        "ssl" => array(
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+            "allow_self_signed" => true
+        )
+    )
+);
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
